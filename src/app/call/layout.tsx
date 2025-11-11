984640e3b56b71fe3d7653f1d05d7834dcdff93c
Dorcas Ibrahim <dorcasibrahim46@gmail.com>
2025-11-11 15:21:49 -0600

    video call

---
+++ src/app/call/layout.tsx
@@ -0,0 +1,15 @@
+import { Children } from "react";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+    return(
+        <div className="h-screen bg-black">
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
